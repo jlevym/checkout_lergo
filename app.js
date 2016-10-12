f9cf0801f561ce7eb2cef40ec41b60debcf4c725
jlevym <jlevym@gmail.com>
2016-10-12 17:01:13 +0300

    feature: display all lessons titles in english on front page
    implement factory to hold data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var myApp = angular.module('myApp',[]);
 
-myApp.controller('lessonController',['$scope', function($scope) {
+myApp.controller('lessonController',['$scope', 'lessonCount', function($scope, $lessonCount ) {
 
 	$scope.name = 'jeffrey';
+    $scope.lessons = fac.lessons;
 }]);
