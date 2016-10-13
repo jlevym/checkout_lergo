591ec8ee76e3492db54f3c1188a185bfe0eb7a96
jlevym <jlevym@gmail.com>
2016-10-13 07:49:34 +0300

    refactor and start adding the lesson count

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var myApp = angular.module('myApp',[]);
 
-myApp.controller('lessonController',['$scope', 'lergoData', function($scope, lergoData ) {
+myApp.controller('lessonController',['$scope', 'lergoData', 'allStats', function($scope, lergoData, allStats ) {
 
 	$scope.name = 'jeffrey';
     $scope.lessons = fac.lessons;
+    $scope.goodlessons = stats.countLessons;
 
 }]);
---
+++ factoriesjs/statsfactory.js
@@ -0,0 +1,39 @@
+myApp.factory('allStats', function(lergoData) {
+	stats = {};
+
+stats.lessons = fac.lessons;
+
+// FOR LERGO - RETURN VALUE TRUE IF MORE THAN 2 QUESTIONS. 
+
+var lessonWithTwoQuestions = 0; // initializes the total amount of lessons with 2 questions to 0
+// countItems checks a lesson to see if there are at least 2 questions returning true / false
+
+var countItems = function(lesson){
+        var questionsInLesson = 0;
+        for (s in lesson.steps) {
+            if(lesson.steps[s].quizItems){ // only dealing with steps that have quiz items. 
+                for (q in lesson.steps[s].quizItems ){
+                    questionsInLesson +=1;
+                        if(questionsInLesson>1){
+                            return true;
+                        }
+                }
+            }
+        } return false;
+}
+
+
+//THIS SUMS THE AMOUNT OF LESSONS THAT HAVE AT LEAST 2 QUESTIONS 
+// by calling the countItems function 
+var countLessons = function(){
+    for (docs in stats.lessons) {
+        if(countItems(lesson[docs])){
+            lessonWithTwoQuestions += 1;
+        }
+    } return lessonWithTwoQuestions ;
+}
+
+
+	status.countLessons = countLessons;
+	return status;
+})
