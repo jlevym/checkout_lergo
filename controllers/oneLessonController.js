6a5fc6022ae139aa048d92019f3e37eeacdb30b3
jlevym <jlevym@gmail.com>
2016-10-17 10:54:34 +0300

    implement single lessons gets the routeparam and shows on it's page

--- controllers/oneLessonController.js
+++ controllers/oneLessonController.js
@@ -1,9 +1,21 @@
 myApp.controller('oneLessonController', ['$scope', '$routeParams','lergoData',   function($scope, $routeParams, lergoData  ) {
 
-
+	var thislesson ={};
     $scope.lessons = fac.lessons;
-    $scope.goto = $routeParams.id;
-    
+    $scope.thislessonid = $routeParams.id; // has the id of the chosen lesson
+    var id = $routeParams.id;
+    var a = function () {
+    	for(var i= 0; i< $scope.lessons.length; i++) {
+    		if($scope.lessons[i]._id == $scope.thislessonid) {
+    			thislesson = $scope.lessons[i];
+    			return thislesson;
+    		}
+    	}
+    	
+    }
+
+    console.log(a());
+    $scope.lessonObj = thislesson;
 
 
 }]);
