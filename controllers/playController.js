6916bf29935a6932472f878fe204efa367a56407
jlevym <jlevym@gmail.com>
2016-10-24 11:48:03 +0300

    clean up comments

--- controllers/playController.js
+++ controllers/playController.js
@@ -10,18 +10,17 @@ myApp.controller('playLessonController',['$scope', 'lergoData', 'lergoQuestions'
 
    	var id = $routeParams.id;  // id of the lesson we want to play
 	$scope.thislesson = alesson(id); // this sends the request for one lesson from lessonFactory
-	/*console.log( 'this lesson from playconroller.js');
-	console.log($scope.thislesson);*/	 // prints out the lesson in the console. 
-
+	
     // this looks for all the questions in a lesson step given by the countlessons(lessonstep) below;
 	var thislesson = $scope.thislesson;
 	var steps = thislesson.steps;
-	var questions = []; // this is an array to keep all the steps / questions
+
+	// this is an empty array to keep all the steps / questions but only of 1 step
+	var questions = []; 
 
 
 	// this function below will extact the questions from the passed in step of the lesson
 	// and make an array for that step called questions
-	// the next step is to play this array
 	var countlessons = function (step) { /*we pass in the step number we want*/
 				if(steps[step].quizItems) {
 					for (quizItem in steps[step].quizItems){
@@ -30,7 +29,7 @@ myApp.controller('playLessonController',['$scope', 'lergoData', 'lergoQuestions'
 			}else {
 				return 'no quizItems in this step';
 			}
-		return questions;
+		return questions; // the array of questions for this step
 		}
 	
 
@@ -46,7 +45,7 @@ myApp.controller('playLessonController',['$scope', 'lergoData', 'lergoQuestions'
 
 	  // play countlessons(3)
 	  	var index = 0;
-	  	$scope.index = index;
+	  	$scope.index = index; // the question in the array of questions
 	  	//this will move us to the next question in the array.
 	  	$scope.increment = function() {
 	  		$scope.index++;
@@ -55,9 +54,9 @@ myApp.controller('playLessonController',['$scope', 'lergoData', 'lergoQuestions'
 	  	}
 
 
-	  	$scope.question = countlessons(3)[$scope.index];	
-	  	$scope.aoptions = 'type4';
-	  	/*var playquestion = function(question) {*/
+	  	$scope.question = countlessons(3)[$scope.index]; // still working with a specific example from  	
+	  	/*$scope.aoptions = 'type4';*/
+	  	
 	  	$scope.aoptions = function(question) {
 	  		var thequestion=aquestion(question); //get the question via the questionid
 	  		$scope.question1=thequestion;
@@ -78,29 +77,9 @@ myApp.controller('playLessonController',['$scope', 'lergoData', 'lergoQuestions'
 		  		return 'type4';
 		  	}
 	  	}
-	  	//console.log(playquestion(countlessons(3)));
-
-	  	// must have something, like this below, to make the playquestion function run the first time.
-
 	  	
-		/*$scope.options = playquestion(countlessons(3)[$scope.index]);*/ // by defining the scope, it causes the function to run
-		
 		$scope.options = $scope.aoptions($scope.question); 
 		
-
-
-
-
-	 /* below is all the code for the truefalsequestion.html*/
-	
-	/*this will give the first question of the lesson that has quizitems*/
-    /*var thisquestion = aquestion(countlessons(3)[0]);*/ // this sends the request for one question from questionFactory
-
-    /*$scope.question1 = thisquestion;*/  // this is just a specific question, not the right one. 
-
-
-    /*console.log( 'this question from playconroller.js');
-    console.log(thisquestion); */
 
     // checks the answer from a true / false and gives the result
     // extracting the value of the clicked radio button
