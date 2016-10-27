ac2300f72e435c2a3a82afe46013b970a691a184
jlevym <jlevym@gmail.com>
2016-10-27 18:31:46 +0300

    implement changes from work sent by email

--- controllers/playController.js
+++ controllers/playController.js
@@ -6,8 +6,7 @@ myApp.controller('playLessonController',['$scope', 'lergoData', 'lergoQuestions'
   }
 
 	// return the lesson with the clicked id
-	$scope.lessons = fac.lessons; // access to all the lessons			do we need this?
-    /*$scope.thislessonid = $routeParams.id;*/ // this specific lesson
+	/*$scope.lessons = fac.lessons;*/ // access to all the lessons			do we need this?
     $scope.questions = qfac.questions; // access to all the questions
    
 
@@ -21,7 +20,7 @@ myApp.controller('playLessonController',['$scope', 'lergoData', 'lergoQuestions'
 	$scope.result = 'please choose an answer'; // toggles between choose an answer or the result of the quiz (correct / not correct)
 	$scope.quizid = ''; // id of a question
 	$scope.options = '';
-	// new code
+	$scope.stepresult = {};
 	// define and set variables
 
 	$scope.increment = function() {
@@ -31,8 +30,8 @@ myApp.controller('playLessonController',['$scope', 'lergoData', 'lergoQuestions'
 		$scope.result = 'please choose an answer'; // toggles between choose an answer or the result of the quiz (correct / not correct)
 		$scope.quizid = ''; // id of a question
 		$scope.options = lessontype();
+	
 	}
-
 
 		/*var lessontype is the variable that changes depending on the step type (video, quiz etc) */
 		 var lessontype = function () { //returns values to playlesson.html
@@ -60,37 +59,52 @@ myApp.controller('playLessonController',['$scope', 'lergoData', 'lergoQuestions'
 							quizindex++;
 							if (quizindex < thistype.quizItems.length) { // tests if we are at the end of the array of questions
 								index = index-1;
+							}else{ // the question array for this step is over
+								$scope.stepresult[index] = quizresultsarray ; //change the name of the array of results
+								console.log("final results of this step: " + quizresultsarray);
+								console.log($scope.stepresult);
+								quizresultsarray =  [];
 							}
 							
 							return type;
 
-						}else{
-							return 'need more definitions'; 
+						}else{ // neither video nor quiz
+							return 'need more definitions'; // other than quiz or video so far
 						}	
 
 				}else{
+					
+					$scope.stepresult[index] = quizresultsarray ; //change the name of the array of result
+					console.log("final results of this step: " + quizresultsarray);
+					console.log($scope.stepresult);
+					quizresultsarray =  [];
 					console.log('lesson is over'); // what to do when lesson is over
 					return 'report';
 				}
 				
 	}
-
+		quizresultsarray = []; //the array that gives the results for each step
     	/*checks the values of the radio button and answer if true / false*/
 	   $scope.newValue = function(value) {
 			$scope.isDisabled = true;
 			$timeout($scope.increment, 1000); // moves to the next question after timeout time	
 			   	if(value === 'True'){ // this is the option for True / False
 			   		$scope.result = 'good job';
+			   		quizresultsarray.push($scope.result);
+			   		console.log(quizresultsarray);
 		     		console.log(value);
 			   	}else if (value.checked){ // these are the options for multipleChoices
 			   		$scope.result = 'good job';
+			   		quizresultsarray.push($scope.result);
+			   		console.log(quizresultsarray);
 		     		console.log(value.checked);
 			   	}else{
 			   		$scope.result = 'sorry, that is incorrect' ;
+			   		quizresultsarray.push($scope.result);
+			   		console.log(quizresultsarray);
 			   		console.log(value);
 	   	}
 }
-
 
 }]);
 
