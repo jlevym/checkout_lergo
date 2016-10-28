418969e3c2cd9a2df9959d9eba17caa284ad6f1c
jlevym <jlevym@gmail.com>
2016-10-28 15:32:17 +0300

    clean up code : remove many console.log

--- controllers/playController.js
+++ controllers/playController.js
@@ -36,28 +36,22 @@ myApp.controller('playLessonController',['$scope', 'lergoData', 'lergoQuestions'
 					var thistype = $scope.thislesson.steps[index];
 					if (thistype)	{ // checks if there is a step at this index
 						if (thistype.type === 'video') {
-							console.log('video');
 							var thisurl = thistype.videoUrl
-							console.log(thisurl);
 							var complianturl= thisurl.replace("http://youtu.be", "https://www.youtube.com/embed");
 							complianturl = complianturl + '?rel=0'
-							console.log(complianturl);
 							$scope.videourl = complianturl;
 							return 'video';
 						}
 
 						else if (thistype.type === 'quiz') {
-							console.log('this is a quiz');
 							$scope.quizItem = thistype.quizItems[quizindex];
 							$scope.question1=aquestion($scope.quizItem);// will return the question from questionsfactory
 							var type = $scope.question1.type;
-							console.log(type); // will give the type of question
 							quizindex++;
 							if (quizindex < thistype.quizItems.length) { // tests if we are at the end of the array of questions
 								index = index-1;
 							}else{ // the question array for this step is over
 								$scope.stepresult[index] = quizresultsarray ; // key of each array is the index number
-								console.log($scope.stepresult);
 								quizresultsarray =  [];
 							}
 							
@@ -70,7 +64,6 @@ myApp.controller('playLessonController',['$scope', 'lergoData', 'lergoQuestions'
 				}else{
 					
 					$scope.stepresult[index] = quizresultsarray ; //add data to the key 'index' value
-					console.log($scope.stepresult);
 					quizresultsarray =  [];
 					console.log('lesson is over'); // what to do when lesson is over
 					$scope.finalgrade = grades.grade(finalArray); // calculated in gradefactory. 
