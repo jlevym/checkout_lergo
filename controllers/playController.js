921965fd566d711780ce7c7cc920d7e83255322c
jlevym <jlevym@gmail.com>
2016-10-28 16:14:32 +0300

    clean up code

--- controllers/playController.js
+++ controllers/playController.js
@@ -27,9 +27,7 @@ myApp.controller('playLessonController',['$scope', 'lergoData', 'lergoQuestions'
 		$scope.isDisabled = false; // allows the user to click on the answer
 		$scope.result = 'please choose an answer'; // toggles between choose an answer or the result of the quiz (correct / not correct)
 		$scope.options = lessontype();
-	
 	}
-
 		/*var lessontype is the variable that changes depending on the step type (video, quiz etc) */
 		 var lessontype = function () { //returns values to playlesson.html
 					index++;
@@ -38,31 +36,27 @@ myApp.controller('playLessonController',['$scope', 'lergoData', 'lergoQuestions'
 						if (thistype.type === 'video') {
 							var thisurl = thistype.videoUrl
 							var complianturl= thisurl.replace("http://youtu.be", "https://www.youtube.com/embed");
-							complianturl = complianturl + '?rel=0'
+							complianturl = complianturl + '?rel=0' // so that it will not show advertizements at end
 							$scope.videourl = complianturl;
 							return 'video';
 						}
-
 						else if (thistype.type === 'quiz') {
 							$scope.quizItem = thistype.quizItems[quizindex];
 							$scope.question1=aquestion($scope.quizItem);// will return the question from questionsfactory
 							var type = $scope.question1.type;
 							quizindex++;
 							if (quizindex < thistype.quizItems.length) { // tests if we are at the end of the array of questions
-								index = index-1;
+								index = index-1; // if not at the end, we want to stay on the same step
 							}else{ // the question array for this step is over
 								$scope.stepresult[index] = quizresultsarray ; // key of each array is the index number
 								quizresultsarray =  [];
 							}
-							
 							return type;
 
 						}else{ // neither video nor quiz
 							return 'need more definitions'; // other than quiz or video so far
 						}	
-
 				}else{
-					
 					$scope.stepresult[index] = quizresultsarray ; //add data to the key 'index' value
 					quizresultsarray =  [];
 					console.log('lesson is over'); // what to do when lesson is over
@@ -87,8 +81,6 @@ myApp.controller('playLessonController',['$scope', 'lergoData', 'lergoQuestions'
 			   		quizresultsarray.push(0);
 	   	}
 }
-
-	
 		var finalArray = $scope.stepresult; // all the results from the lesson
 		// the grade for all the questions will be calculated in gradefactory
 }]);
